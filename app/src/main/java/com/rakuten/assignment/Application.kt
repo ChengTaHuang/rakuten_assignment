d35fa3fb1b70e8564bb8256b5cef9b8e133bc8b6
zenoDinDinn <h.cta.dindinn@gmail.com>
2020-07-15 19:18:36 +0800

    add basic library

---
+++ app/src/main/java/com/rakuten/assignment/Application.kt
@@ -0,0 +1,10 @@
+package com.rakuten.assignment
+
+import androidx.multidex.MultiDexApplication
+
+class Application : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
